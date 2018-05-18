ed3d295d3b4d4372affbab880f02c794339f9bb1
Viktor Shaposhnikov <kassini_90@mail.ru>
2018-05-18 18:23:08 +0300

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function (){
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'andrew',
-        text: 'hello from client'
-    });
 });
 
 socket.on('disconnect', function (){
